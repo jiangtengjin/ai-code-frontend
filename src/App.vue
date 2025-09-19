3202bca3bc262a371b9a7480e1e0a9e418fee3e0
jiangtengjin <jiangtengjin@appiron.cn>
2025-09-19 15:01:53 +0800

    feat:前端代码初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+
+import { health } from '@/api/healthController.ts'
+
+health().then((res) => {
+  console.log(res)
+})
+</script>
+
+<template>
+  <BasicLayout />
+</template>
+
+<style>
+</style>
